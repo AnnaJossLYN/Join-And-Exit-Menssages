9169436c7b24ef1fdec28617dc618991339f7fe7
AnnaJossLYN <87102613+AnnaJossLYN@users.noreply.github.com>
2021-07-07 20:39:30 -0500

    Update Main.php

--- src/AnnaJossLYN/Main.php
+++ src/AnnaJossLYN/Main.php
@@ -9,15 +9,19 @@ use pocketmine\event\player\PlayerQuitEvent;
 
 class Main extends PB implements L {
 
+        public function onEnable(){ 
+          $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        }
+
 	public function onJoin(PlayerJoinEvent $event){
 	   $player = $event->getPlayer();
-	   $event->setJoinMessage("§a".$player->getName());
+	   $event->setJoinMessage("§a".$player->getName()."Join The Server");
 
 	}
 	
 	public function onExit(PlayerQuitEvent $event){
            $player = $event->getPlayer();
-           $event->setQuitMessage("§c".$player->getName());
+           $event->setQuitMessage("§c".$player->getName()."Exit The Server");
 	}
 	
 }
