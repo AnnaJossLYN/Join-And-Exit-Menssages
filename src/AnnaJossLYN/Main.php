960d70399f6b98280c6cc6c17c4bd1ec1f30496a
AnnaJossLYN <87102613+AnnaJossLYN@users.noreply.github.com>
2021-07-07 20:33:48 -0500

    Quit Config Coming soon

--- src/AnnaJossLYN/Main.php
+++ src/AnnaJossLYN/Main.php
@@ -4,31 +4,20 @@ namespace AnnaJossLYN;
 
 use pocketmine\plugin\PluginBase as PB;
 use pocketmine\event\Listener as L;
-use pocketmine\utils\Config;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 
 class Main extends PB implements L {
-	
-	public function onEnable(){
-            $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-            $this->saveResource("config.yml");
-            $this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
-	
+
 	public function onJoin(PlayerJoinEvent $event){
-		$player = $event->getPlayer();
-                $name->getName();     
-                $msg = $this->getConfig()->get("JoinMessage");
-		$event->setJoinMessage(str_replace([{name}], [$name], $msg)));
+	   $player = $event->getPlayer();
+	   $event->setJoinMessage("§a".$player->getName());
 
 	}
 	
 	public function onExit(PlayerQuitEvent $event){
-		$player = $event->getPlayer();
-                $name->getName();
-                $msg = $this->getConfig()->get("ExitMessage");
-		$event->setQuitMessage(str_replace([{name}], [$name], $msg)));
+           $player = $event->getPlayer();
+           $event->setQuitMessage("§c".$player->getName());
 	}
 	
 }
